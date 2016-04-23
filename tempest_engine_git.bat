99850b68e001b83990d53cf90aa1a5064137f92d
Sornemus <sornemus@ukr.net>
2016-04-23 18:36:22 +0300

    test commit

--- tempest_engine_git.bat
+++ tempest_engine_git.bat
@@ -1,3 +1,9 @@
+::git add *.*
+::git commit -m "test commit"
+::git remote add origin https://github.com/Sornemus/tempest_engine.git
+::git push -u origin master
+git status
 git add *.*
-git commit -m "initial project version"
+git commit -m "test commit"
+git push origin master
 pause
